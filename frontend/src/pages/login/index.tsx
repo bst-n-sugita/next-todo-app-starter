168739d740a1e3682aec5d325d52f4b8357b58db
Nao Sugita <n.sugita@buysell-technologies.com>
2022-07-17 18:23:00 +0900

    ログイン中に/loginにアクセスしたらrootにリダイレクト

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,13 +1,28 @@
+import { useEffect } from "react";
+
+import { useAuth0 } from "@auth0/auth0-react";
 import { Container } from "@mui/material";
+import router from "next/router";
 
 import Header from "../../components/organisms/header";
 
 const Login = () => {
+  const { isLoading, isAuthenticated } = useAuth0();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.replace("/");
+    }
+  }, [isAuthenticated]);
+
   return (
-    <>
-      <Header />
-      <Container maxWidth="sm">やることをきれいに管理しよう！</Container>
-    </>
+    !isLoading &&
+    !isAuthenticated && (
+      <>
+        <Header />
+        <Container maxWidth="sm">やることをきれいに管理しよう！</Container>
+      </>
+    )
   );
 };
 
